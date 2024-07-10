5a5c4db79f624acb5af54ab26b974694dcd0fff6
Daniel <daniel.ddhm@gmail.com>
2024-07-10 03:43:57 -0300

    feat: implementation of initial routes and db items (#1)
    * feat: initial
    
    * fix: queries

--- internal/application.go
+++ internal/application.go
@@ -3,6 +3,7 @@ package internal
 import (
 	charmLog "github.com/charmbracelet/log"
 	"github.com/gorilla/mux"
+	"github.com/japhy-tech/backend-test/internal/handlers"
 )
 
 type App struct {
@@ -16,5 +17,9 @@ func NewApp(logger *charmLog.Logger) *App {
 }
 
 func (a *App) RegisterRoutes(r *mux.Router) {
-	//TODO: Implement routes
+	r.HandleFunc("/breeds", handlers.GetBreeds).Methods("GET")
+	r.HandleFunc("/breeds/{id}", handlers.GetBreed).Methods("GET")
+	r.HandleFunc("/breeds", handlers.CreateBreed).Methods("POST")
+	r.HandleFunc("/breeds/{id}", handlers.UpdateBreed).Methods("PUT")
+	r.HandleFunc("/breeds/{id}", handlers.DeleteBreed).Methods("DELETE")
 }
